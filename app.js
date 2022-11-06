71128b7bb0c53c6c50795a84cc6c11287cc28d81
Chaytan Arora <chaytanarora@gmail.com>
2022-11-06 23:08:31 +0530

    implemented login functionality using passport

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const exphbs = require("express-handlebars");
+const passport = require("passport");
+const session = require("express-session");
 const connectDB = require("./config/db");
 
 //Load config
 dotenv.config({ path: "./config/config.env" });
 
 connectDB();
+
+// Passport config
+require("./config/passport")(passport);
 
 const app = express();
 
@@ -24,8 +29,22 @@ app.use(express.static(path.join(__dirname, "public")));
 app.engine(".hbs", exphbs.engine({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", ".hbs");
 
+// Session middleware
+app.use(
+	session({
+		secret: "chaytan",
+		resave: false,
+		saveUninitialized: false,
+	})
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Routes
 app.use("/", require("./routes/index"));
+app.use("/auth", require("./routes/auth"));
 
 const PORT = process.env.PORT || 5000;
 
---
+++ routes/auth.js
@@ -0,0 +1,28 @@
+const passport = require("passport");
+const router = require("express").Router();
+
+// @desc  Auth with google
+// @route GET /auth/google
+
+router.get("/google", passport.authenticate("google", { scope: ["profile"] }));
+
+// @desc  Auth with google callback
+// @route GET /auth/google/callback
+
+router.get(
+	"/google/callback",
+	passport.authenticate("google", { failureRedirect: "/" }),
+	(req, res) => {
+		res.redirect("/dashboard");
+	}
+);
+
+// @desc  Logout
+// @route GET /auth/logout
+
+router.get("/logout", (req, res) => {
+	req.logOut();
+	res.redirect("/");
+});
+
+module.exports = router;
